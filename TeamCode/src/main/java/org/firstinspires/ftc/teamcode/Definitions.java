24c939d209d9ca8254b4c981881b39f18532f02e
Samuel Carter <SamuelPCarter@gmail.com>
2019-10-02 08:54:07 -0700

    Forgot to add all files to last commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Definitions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Definitions.java
@@ -1,4 +1,41 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Definitions {
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class is used to define all the specific hardware for a single robot.
+ * In this case that robot is a Mecanum driven Robot.
+ * Mecanum robots utilize 4 motor driven wheels. Each of these wheels 
+ * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
+ *
+ *
+ */
+public class Definitions
+{
+    public DcMotor leftBackMotor;
+    public DcMotor leftFrontMotor;
+    public DcMotor rightBackMotor;
+    public DcMotor rightFrontMotor;
+
+    public void robotHardwareMapInit(HardwareMap Map)
+    {
+        leftBackMotor = Map.dcMotor.get("leftBackMotor");
+        leftFrontMotor = Map.dcMotor.get("leftFrontMotor");
+        rightBackMotor = Map.dcMotor.get("rightBackMotor");
+        rightFrontMotor = Map.dcMotor.get("rightFrontMotor");
+    }
+
+    void runWithOutEncoders()
+    {
+        leftFrontMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBackMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFrontMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBackMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
 }
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
@@ -1,4 +1,72 @@
 package org.firstinspires.ftc.teamcode;
 
-public class TeleOP {
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * Lets hope this shows up on git!
+ */
+
+@TeleOp(name="TeleOP - Iterative")
+
+public class TeleOP extends OpMode
+{
+    Definitions robot = new Definitions();
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        robot.robotHardwareMapInit(hardwareMap);
+        robot.runWithOutEncoders();
+        robot.leftBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        robot.leftFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        robot.rightBackMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        robot.rightFrontMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start(){
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        robot.rightFrontMotor.setPower(Range.clip((-gamepad1.left_stick_y - (gamepad1.left_stick_x) - gamepad1.right_stick_x), -1, 1));
+        robot.leftFrontMotor.setPower(Range.clip((gamepad1.left_stick_y - (gamepad1.left_stick_x) - gamepad1.right_stick_x), -1, 1));
+        robot.rightBackMotor.setPower(Range.clip((-gamepad1.left_stick_y + (gamepad1.left_stick_x) - gamepad1.right_stick_x), -1, 1));
+        robot.leftBackMotor.setPower(Range.clip((gamepad1.left_stick_y + (gamepad1.left_stick_x) - gamepad1.right_stick_x), -1, 1));
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
 }
