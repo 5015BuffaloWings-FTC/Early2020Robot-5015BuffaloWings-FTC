fe882c2f559b4ff2afa6a7ffee1660e55ad4ccdb
Samuel Carter <SamuelPCarter@gmail.com>
2019-11-27 11:51:47 -0800

    Last Commit of test robot. Currently Works.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Definitions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Definitions.java
@@ -20,16 +20,18 @@ public class Definitions
     public DcMotor leftFrontMotor;
     public DcMotor rightBackMotor;
     public DcMotor rightFrontMotor;
-    public DcMotor leftIntake;
-    public DcMotor rightIntake;
+//    public DcMotor leftIntake;
+//    public DcMotor rightIntake;
 
     public void robotHardwareMapInit(HardwareMap Map)
     {
         leftBackMotor = Map.dcMotor.get("leftBackMotor");
         leftFrontMotor = Map.dcMotor.get("leftFrontMotor");
+        rightFrontMotor = Map.dcMotor.get("rightFrontMotor");
         rightBackMotor = Map.dcMotor.get("rightBackMotor");
-        leftIntake = Map.dcMotor.get("leftIntake");
-        rightIntake = Map.dcMotor.get("rightIntake");
+
+//        leftIntake = Map.dcMotor.get("leftIntake");
+//        rightIntake = Map.dcMotor.get("rightIntake");
     }
 
     void teleOpInit()
@@ -38,18 +40,16 @@ public class Definitions
         leftBackMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightFrontMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightBackMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        leftIntake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightIntake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        leftIntake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        rightIntake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         leftBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         leftFrontMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         rightBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         rightFrontMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        leftIntake.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightIntake.setDirection(DcMotorSimple.Direction.FORWARD);
+//        leftIntake.setDirection(DcMotorSimple.Direction.REVERSE);
+//        rightIntake.setDirection(DcMotorSimple.Direction.FORWARD);
     }
-}
+}//2240 ticks Aaed4ln/////AAABmS8KgU/Rvk8jpcgMg/A8+Iop8Zc9/OW+2Cs/fUVcQ83W1DCtxB+B9mgxrXp6l3UpeQi3TsPOOkJEaFKm1TJTuIe7RjPTWBMZLsY+YFQHjkb/0b2o0MRZfUV0rCMc/JcTDjz2e2s4dkhdx+EZt/uFQhOaYHV7jQplXiQTzFgv3jwADOEzdf7xzgehFTxj/p/IVxURKeHkcKRNxghiwtrBSC/t+E8+9se/oHe2eejxAtOltuwGsN0XPmA/Cmq6+IAgltLm/KQXo84VsesCQSb41AamATz9aKoLEpWYPP5qRl9fetXMUwvpmtijN6Qv9sHtABApOCv1GMSiaos3Unu6STz5jznJJbfSI7lnTR8sVQls
 
-
-//last half of tele op init code could be put into robot hardware map init. Also reverse the direction of one of the intake motors in the same method. This code will be at the end.
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
@@ -78,26 +78,26 @@ public class TeleOP extends OpMode
         robot.rightBackMotor.setPower(Range.clip((-gamepad1.left_stick_y + (gamepad1.left_stick_x) - gamepad1.right_stick_x), -1, 1));
         robot.leftBackMotor.setPower(Range.clip((gamepad1.left_stick_y + (gamepad1.left_stick_x) - gamepad1.right_stick_x), -1, 1));
 
-        //Intake
-        if(gamepad1.dpad_up || gamepad1.dpad_down)
-        {
-            if(gamepad1.dpad_down)
-            {
-                robot.leftIntake.setPower(1);
-                robot.rightIntake.setPower(1);
-            }
-            else
-            {
-                robot.leftIntake.setPower(-1);
-                robot.rightIntake.setPower(-1);
-            }
-
-        }
-        else
-        {
-            robot.leftIntake.setPower(0);
-            robot.rightIntake.setPower(0);
-        }
+//        //Intake
+//        if(gamepad1.dpad_up || gamepad1.dpad_down)
+//        {
+//            if(gamepad1.dpad_down)
+//            {
+//                robot.leftIntake.setPower(1);
+//                robot.rightIntake.setPower(1);
+//            }
+//            else
+//            {
+//                robot.leftIntake.setPower(-1);
+//                robot.rightIntake.setPower(-1);
+//            }
+//
+//        }
+//        else
+//        {
+//            robot.leftIntake.setPower(0);
+//            robot.rightIntake.setPower(0);
+//        }
     }
 
     /*
